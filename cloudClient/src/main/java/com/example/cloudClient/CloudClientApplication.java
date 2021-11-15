4ec4a9bb06b46f6020c52ae5fe0b731765c116f6
Joselu-code-clean <71699651+Joselu-code-clean@users.noreply.github.com>
2021-11-15 14:29:19 +0100

    Create profiles dev and prod

--- cloudClient/src/main/java/com/example/cloudClient/CloudClientApplication.java
+++ cloudClient/src/main/java/com/example/cloudClient/CloudClientApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CloudClientApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CloudClientApplication.class, args);
+		
 	}
 
 }
--- cloudClient/src/main/java/com/example/cloudClient/controllers/TestController.java
+++ cloudClient/src/main/java/com/example/cloudClient/controllers/TestController.java
@@ -6,12 +6,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-
+	
+	
+	@Value("${spring.profiles.active:}")
+	private String activeProfiles;
+	
 	@Value("${some.value}")
 	private String myValue;
 	
 	@GetMapping(path = "/myValue")
 	public String myValue() {
+		for (String profileName : activeProfiles.split(",")) {
+            if (profileName.equals("dev")) {
+            	this.myValue = "Hola Desarrollador";
+			} else if (profileName.equals("prod")) {
+				this.myValue = "Hola tester";
+			}
+        }
 		return this.myValue;
 	}
 	
